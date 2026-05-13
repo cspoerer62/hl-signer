e686ef9d3735fd003401a31fe98c66d2f8ad7a01
cspoerer62 <411voip@gmail.com>
2026-05-13 14:19:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-{
-  "name": "hl-signer",
-  "version": "2.0.0",
-  "main": "index.js",
-  "scripts": {"start": "node index.js"},
-  "dependencies": {
-    "express": "^4.18.0",
-    "ethers": "^6.0.0",
-    "@msgpack/msgpack": "^3.0.0-beta2"
-  }
-}
+const express = require('express');
+const { ethers } = require('ethers');
+const msgpack = require('@msgpack/msgpack');
